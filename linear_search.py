86ea2af9a6fb06c807ff2911cef2a859ae187b3a
Mohibakhon <89140008+mokhibakhon@users.noreply.github.com>
2024-05-31 18:51:57 +0500

    Add files via upload

---
+++ linear_search.py
@@ -0,0 +1,19 @@
+import random
+import time
+from datetime import datetime
+
+data = []
+for i in range(20):
+    data.append(random.randint(1, 50))
+print(data)
+target = int(input("target = "))
+count = 0
+print(datetime.now())
+for j in data:
+    time.sleep(1)
+    count += 1
+    if j == target:
+        print(target)
+        break
+print(count)
+print(datetime.now())
